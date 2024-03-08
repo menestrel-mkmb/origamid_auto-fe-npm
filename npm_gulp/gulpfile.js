f006291ed0c368013c29710162564752213a2bd1
Michael Kevin <michaelkmb@hotmail.com>
2024-03-08 16:44:30 -0300

    misc using watch to live compile sass;

--- npm_gulp/gulpfile.js
+++ npm_gulp/gulpfile.js
@@ -1,23 +1,29 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('node-sass'));
 
-// function sassCompile() {
-//     return gulp.src('css/scss/**/*.scss')
-//     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-//     .pipe(gulp.dest('css/'))
-// }
-
-// gulp.task('sass', sassCompile);
-
-gulp.task('sass', async function() {
-    import('gulp-autoprefixer').then(module => {
-      const autoPrefixer = module.default;
-      return gulp.src('css/scss/**/*.scss')
+function sassCompileOld() {
+    return gulp.src('css/scss/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(autoPrefixer())
     .pipe(gulp.dest('css/'))
-      // Your existing code that uses autoPrefixer
-    });
-  
-    // Other code related to the 'sass' task
+}
+
+gulp.task('sass-old', sassCompileOld);
+
+async function sassCompile() {
+  import('gulp-autoprefixer').then(module => {
+    const autoPrefixer = module.default;
+    return gulp.src('css/scss/**/*.scss')
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(autoPrefixer())
+  .pipe(gulp.dest('css/'))
+    // Your existing code that uses autoPrefixer
   });
+}
+
+gulp.task('sass', sassCompile);
+
+function watch(){
+  gulp.watch('css/scss/**/*.scss', sassCompile);
+}
+
+gulp.task('default', watch);
