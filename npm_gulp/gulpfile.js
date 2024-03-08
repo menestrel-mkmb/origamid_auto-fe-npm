64c54f4a5b6f084d2cd4dd9deda3896ae8d62500
Michael Kevin <michaelkmb@hotmail.com>
2024-03-08 16:11:41 -0300

    misc using autoprefixer to browserList;

--- npm_gulp/gulpfile.js
+++ npm_gulp/gulpfile.js
@@ -1,10 +1,23 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('node-sass'));
 
-function sassCompile() {
-    return gulp.src('css/scss/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('css/'))
-}
+// function sassCompile() {
+//     return gulp.src('css/scss/**/*.scss')
+//     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+//     .pipe(gulp.dest('css/'))
+// }
 
-gulp.task('sass', sassCompile);
+// gulp.task('sass', sassCompile);
+
+gulp.task('sass', async function() {
+    import('gulp-autoprefixer').then(module => {
+      const autoPrefixer = module.default;
+      return gulp.src('css/scss/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoPrefixer())
+    .pipe(gulp.dest('css/'))
+      // Your existing code that uses autoPrefixer
+    });
+  
+    // Other code related to the 'sass' task
+  });
