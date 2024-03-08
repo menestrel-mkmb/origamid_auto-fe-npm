c48fec88285c015c5c761f05401fe18df2ed22b5
Michael Kevin <michaelkmb@hotmail.com>
2024-03-08 18:04:16 -0300

    misc gulpfile on modal/;

---
+++ npm_gulp/modal/gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('node-sass'));
+const browsersync = require('browser-sync').create();
+
+function sassCompileOld() {
+    return gulp.src('css/scss/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('css/'))
+}
+
+gulp.task('sass-old', sassCompileOld);
+
+async function sassCompile() {
+  import('gulp-autoprefixer').then(module => {
+    const autoPrefixer = module.default;
+    return gulp.src('css/scss/**/*.scss')
+  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+  .pipe(autoPrefixer())
+  .pipe(gulp.dest('css/'))
+  .pipe(browsersync.stream());
+  });
+}
+
+gulp.task('sass', sassCompile);
+
+function watch(){
+  gulp.watch('css/scss/**/*.scss', sassCompile);
+}
+
+gulp.task('watch', watch);
+
+function browser() {
+  browsersync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+}
+
+gulp.task('liveserver', browser);
+
+gulp.task('default', gulp.parallel('watch', 'liveserver'));
