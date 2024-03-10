451653b75f16f429567fe9c08e2f68df078d4344
Michael Kevin <michaelkmb@hotmail.com>
2024-03-09 21:28:55 -0300

    feat jimp script to multiple files;

--- jimp/jimp.js
+++ jimp/jimp.js
@@ -1,11 +1,16 @@
 const Jimp = require('jimp');
+const fs = require('fs');
 
-Jimp.read('./full-imgs/pizza.jpg')
+const images = fs.readdirSync('./full-imgs/');
+
+images.forEach( image => {
+    Jimp.read('./full-imgs/' + image)
     .then( img => {
         img
         .resize(300, Jimp.AUTO) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
-        .write('./thumb-imgs/pizza-thumb.jpg');
+        .write('./thumb-imgs/' + image);
     })
     .catch(err => console.log(err));
+})
